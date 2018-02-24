b7c497a51da4bb8a390be17c98085e52550bd970
Colin <cwm@eml.cc>
2018-02-24 12:09:02 -0500

    Refactor and update for new scapy packages
    Changes in scapy made the old code unstable.
    New changes should more reliably catch all dash pushes
    
    Updated init.d scripts to accomodate refactoring
    
    arpscanner.py added to help find new dash buttons on network
    
    uses datacollectionbot to update states in IOSMaster

--- housescapy/housescapy.py
+++ housescapy/housescapy.py
@@ -11,17 +11,18 @@ import argparse
 import sys
 import time
 
-sys.path.append('/home/leibert/pyScripts')
-from datacollectionbot.databot import *
+sys.path.append('/opt/datacollectionbot/')
+from databot import *
 
 
 # Deafults
-LOG_FILENAME = "/tmp/coffee.log"
+LOG_FILENAME = "/var/log/housescapy.log"
 LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
 
 # Define and parse command line arguments
 parser = argparse.ArgumentParser(description="My simple Python service")
-parser.add_argument("-l", "--log", help="file to write log to (default '" + LOG_FILENAME + "')")
+parser.add_argument(
+    "-l", "--log", help="file to write log to (default '" + LOG_FILENAME + "')")
 
 # If the log file is specified on the command line then override the default
 args = parser.parse_args()
@@ -34,7 +35,8 @@ logger = logging.getLogger(__name__)
 # Set the log level to LOG_LEVEL
 logger.setLevel(LOG_LEVEL)
 # Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
-handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+handler = logging.handlers.TimedRotatingFileHandler(
+    LOG_FILENAME, when="midnight", backupCount=3)
 # Format each log message like this
 formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
 # Attach the formatter to the handler
@@ -43,6 +45,8 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # lastminute = 0  # holder for minute timer
+
+updateState("test", "TS")
 
 
 # Make a class we can use to capture stdout and sterr in the log
@@ -57,6 +61,7 @@ class MyLogger(object):
         if message.rstrip() != "":
             self.logger.log(self.level, message.rstrip())
 
+
 # Replace stdout with logging to file at INFO level
 sys.stdout = MyLogger(logger, logging.INFO)
 # Replace stderr with logging to file at ERROR level
@@ -64,76 +69,74 @@ sys.stderr = MyLogger(logger, logging.ERROR)
 
 i = 0
 
-def arp_display(pkt):
+
+def detect_button(pkt):
     # global lastminute
     # if time.localtime().tm_min != lastminute:
     #     lastminute=time.localtime().tm_min
     #     updateCMDCTRL()
+    # print all ARP packets. This is helpful to find new das buttons
+#    if pkt[ARP].op == 1 and pkt.haslayer(ARP) and pkt[ARP].psrc == '0.0.0.0':
+#        print "ARP Packet detected"
+#        print pkt[ARP].hwsrc
 
-    if pkt[ARP].op == 1:  #who-has (request)
-
-        #		print 'whois'
-        if pkt[ARP].psrc == '0.0.0.0':  # ARP Probe
-            print "ARP Probe from: " + pkt[ARP].hwsrc
-
-        if pkt[ARP].hwsrc == '00:bb:3a:6f:02:21':
+    # we only care about DHCP packets
+    if pkt.haslayer(DHCP):
+        # start checking source of packets to see if they came from a known dash button
+        if pkt[Ether].src == '00:bb:3a:6f:02:21':
+            print "DASH 02:21 Detected - TEST "
             print time.time()
-            print 'ON DASH A'
+
             try:
-                r = requests.get("http://192.168.0.31/cgi-bin/IOS/ios.py?mode=execmacro&macroID=4")
+                r = requests.post(
+                    "http://192.168.0.31/cgi-bin/IOS/ios.py?mode=execmacro&macroID=4", data={'submit': 'randomshit'})
                 print(r.status_code, r.reason)
                 print(r.text)
                 time.sleep(5)
+
             except:
                 print 'Backyard light error'
 
-        elif pkt[ARP].hwsrc == '10:ae:60:f0:ce:0a':
-	        print time.time()
-            print 'coffee dash!/old DASH B'
-            updateState("coffee","TS")
+        elif pkt[Ether].src == '10:ae:60:f0:ce:0a':
+            print 'DASH f0:ce:0a Detected - TEST BUTTON'
+            print time.time()
+
+        elif pkt[Ether].src == '44:65:0d:d9:c3:97':
+            print 'd9:c3:97 Detected - TEST BUTTON 2'
+            print time.time()
+
+
+        elif pkt[Ether].src == '10:ae:60:f0:ce:0b':
+            print 'DASH f0:ce:0b Detected - LOWER PORCH BUTTON'
+            print time.time()
+            updateState("DASHB", "TS")
+
             try:
-                r = requests.post("http://192.168.0.30/coffee.php?potid=fl1", data={'submit': 'randomshit'})
+                r = requests.post(
+                    "http://192.168.0.0/LIGHTS=TOGGLE", data={'submit': 'randomshit'})
+                print(r.status_code, r.reason)
+                print(r.text)
+        # sleep to debounce
+                time.sleep(5)
+            except:
+                print 'IOS Server Error'
+
+        elif pkt[Ether].src == '44:65:0d:26:a9:35':
+            print 'DASH 26:a9:35 Detected - COFFEE'
+            print time.time()
+            updateState("coffee", "TS")
+
+            try:
+                r = requests.get("http://192.168.0.15/coffeemadeGOGOGO")
                 print(r.status_code, r.reason)
                 print(r.text)
             except:
-                print 'coffee server error'
-
-            try:
-                r = requests.post("http://192.168.0.50/coffeemadeGOGOGO", data={'submit': 'randomshit'})
-                print(r.status_code, r.reason)
-                print(r.text)
-            except:
-                print 'lightESP error'
-            
-
-
-       # elif pkt[ARP].hwsrc == '74:c2:46:5b:8b:a4': BATTERY DIED
-#            print time.time()
- #           print 'coffee dash!'
-#	    updateState("coffee","TS")
- #           try:
-  #              r = requests.post("http://192.168.0.30/coffee.php?potid=fl1", data={'submit': 'randomshit'})
-   #             print(r.status_code, r.reason)
-    #            print(r.text)
-     #       except:
-      #          print 'coffee server error'
-#
- #           try:
-  #              r = requests.post("http://192.168.0.50/coffeemadeGOGOGO", data={'submit': 'randomshit'})
-   #             print(r.status_code, r.reason)
-    #            print(r.text)
-     #       except:
-      #          print 'lightESP error'
+                print 'error contacting 0.15'
 
 
 # Loop forever, doing something useful hopefully:
 # while True:
 # program loop continuously running
 while True:
-    print sniff(prn=arp_display, filter="arp", store=0, count=0)
+    sniff(prn=detect_button, filter="(udp and (port 67 or 68))", store=0)
     # sleep(1000)
-
-
-
-
-
